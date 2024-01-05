7610ff61ed734ce3f311687e03f9dc26f5950b22
Yonle <yonle@lecturify.net>
2024-01-05 18:02:40 +0700

    clusters, number of pools, fixes
    Signed-off-by: Yonle <yonle@lecturify.net>

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const NostrTools = require("nostr-tools");
 const bodyParser = require("body-parser");
 
 const app = express();
-const pool = new NostrTools.SimplePool();
+const pools = [];
 
 const config = require("./config");
 
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 
 app.get("/req", async (req, res) => {
   const filter = req.query;
+  const pool = getPool();
   filter.limit = parseInt(((filter.limit > config.req_limit) ? config.req_limit : filter.limit) || config.req_limit);
   filter.authors = filter?.authors?.split(",") || [];
   filter.kinds = filter?.kinds?.split(",")?.map(parseInt) || [];
@@ -39,14 +40,23 @@ app.get("/req", async (req, res) => {
     });
   }
 });
-
+7
 app.post("/publish", async (req, res) => {
-  if (!NostrTools.validateEvent(req.body) || !NostrTools.verifySignature(req.body)) return res.status(400).json({ status: 1, error: "Invalid event" });
+  if (!NostrTools.validateEvent(req.body) || !NostrTools.verifyEvent(req.body)) return res.status(400).json({ status: 1, error: "Invalid event" });
+  const pool = getPool();
   // it does not guarantee.
   pool.publish(config.relays, req.body);
-
   res.json({ status: 0, error: null });
 });
 
+function getPool() {
+  const pool = pools.shift();
+  pools.push(pool);
+  return pool;
+}
+
 const listener = app.listen(config.port, _ =>
   console.log(`Now listening on port ${listener.address().port}`));
+
+Array.apply(0, Array(config.pools)).forEach(_ =>
+  pools.push(new NostrTools.SimplePool()));
--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+const config = require("./config");
+const cluster = require("cluster");
+const os = require("os");
+
+if (!process.env.NO_CLUSTERS && cluster.isPrimary) {
+  const numClusters = process.env.CLUSTERS || config.clusters || (os.availableParallelism ? os.availableParallelism() : (os.cpus().length || 2))
+
+  console.log(`Primary ${process.pid} is running. Will fork ${numClusters} clusters.`);
+
+  // Fork workers.
+  for (let i = 0; i < numClusters; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Forking another one....`);
+    cluster.fork();
+  });
+
+  return true;
+}
 
 // only for loading websocket-polyfill and other.
 // check app.js
 import("websocket-polyfill").then(_ => require("./app.js"));
 
-process.on('unhandledRejection', Function());
+process.on('unhandledRejection', console.error);
